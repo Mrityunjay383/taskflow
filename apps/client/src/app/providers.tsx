72325869eb6bf987c1e60b15f93fe606b371b74e
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2026-07-20 03:36:09 +0530

    auth hadling all done

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -1,13 +1,8 @@
 "use client";
 import { ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "@/providers/auth-provider";
 import { queryClient } from "@/lib/queryClient";
 
 export default function Providers({ children }: { children: ReactNode }) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>{children}</AuthProvider>
-        </QueryClientProvider>
-    );
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
--- apps/client/src/providers/auth-provider.tsx
+++ apps/client/src/providers/auth-provider.tsx
@@ -1,41 +1,32 @@
 "use client";
 
-import { createContext, ReactNode, useContext, useEffect, useMemo } from "react";
+import { createContext, ReactNode, useContext, useMemo } from "react";
 import { useCurrentUser } from "@/features/auth/auth.queries";
 import { AuthContextValue } from "@/lib/types";
-import { useRouter } from "next/navigation";
 
 const AuthContext = createContext<AuthContextValue | null>(null);
 
-export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const router = useRouter();
-
+export function AuthProvider({ children }: { children: ReactNode }) {
     const query = useCurrentUser();
 
-    const value = useMemo(
+    const value = useMemo<AuthContextValue>(
         () => ({
             user: query.data ?? null,
             isLoading: query.isPending,
             isAuthenticated: !!query.data,
         }),
-        [query],
+        [query.data, query.isPending],
     );
 
-    useEffect(() => {
-        if (!value.isLoading && !value.isAuthenticated) {
-            router.replace("/login");
-        }
-    }, [router, value]);
-
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+}
 
-export const useAuth = () => {
+export function useAuth() {
     const context = useContext(AuthContext);
 
     if (!context) {
-        throw new Error("useAuth must be used within AuthProvider");
+        throw new Error("useAuth must be used inside AuthProvider");
     }
 
     return context;
-};
+}
